d1ef9dd95b23321a1f1fb33ec5026e8c7300a5ee
Justin Heideman <justin@fiddlyio.com>
2014-01-05 13:00:48 -0600

    fv data working

--- js/app/gpfv.js
+++ js/app/gpfv.js
@@ -16,61 +16,43 @@ define(["jquery", 'd3'], function generalProgram($, d3) {
 
 		init: function(selector,path){
 			this.selector = selector;
-
 			this.setupSVG();
-
-			this.doWork(path);
+			this.doApprovedDenied(path);
 
 		},
 
 		setupSVG: function(){
 
-			this.width =  document.body.clientWidth,
-			this.height = $(this.selector).height(),
+			this.width =  document.body.clientWidth;
+			this.height = $(this.selector).height();
 			this.ratio = this.height/$(window).height();  //get the ratio we should use for svg transformation
 
 			this.x = d3.time.scale().range([0, this.width]);
-
 			this.y = d3.scale.linear().range([this.maxHeight, 0]);
 
-
-		}, 
-
-		doWork: function(path){
-			var self = this;
-			// var margin = {top: 0, right: 0, bottom: 0, left: 0},
-			// 	width =  document.body.clientWidth,
-			// 	height = $(this.selector).height(),
-			// 	ratio = height/$(window).height();  //get the ratio we should use for svg transformation
-
-
-			// console.log(height, width);
-
-			// var maxHeight = 300;
-
-
-			// var x = d3.time.scale()
-			// 	.range([0, this.width]);
-
-			// var y = d3.scale.linear()
-			// 	.range([this.maxHeight, 0]);
-
-			var stack = d3.layout.stack()
+			this.stack = d3.layout.stack()
 				.offset("zero")
 				.values(function(d) { return d.values; })
 				.x(function(d) { return d.date; })
 				.y(function(d) { return d.number; });
 
-			 
-			var area = d3.svg.area()
-				.interpolate("cardinal")
-				.x(function(d) { return self.x(d.year); })
-				.y0(function(d) { return self.y(d.y0); })
-				.y1(function(d) { return self.y(d.y0 + d.y); });
 
-			var svg = d3.select(this.selector).append("svg")
+			this.area = d3.svg.area()
+				.interpolate("cardinal")
+				.x(function(d) { return this.x(d.year); })
+				.y0(function(d) { return this.y(d.y0); })
+				.y1(function(d) { return this.y(d.y0 + d.y); });
+
+
+			this.svg = d3.select(this.selector).append("svg")
 				.attr("width", this.width )
 				.attr("height", this.height);
+
+
+		},
+
+		doApprovedDenied: function(path){
+			var self = this;
 
 			d3.csv("/data/"+path+"/granted_denied.csv", function(error, data) {
 				data.forEach(function(d) {
@@ -79,16 +61,16 @@ define(["jquery", 'd3'], function generalProgram($, d3) {
 				});
 				//console.log(data);
 
-				var layers = stack(nest.entries(data));
+				var layers = self.stack(nest.entries(data));
 
 				self.x.domain(d3.extent(data, function(d) { return d.year; }));
 				self.y.domain([0, d3.max(data, function(d) { return d.y0 + d.y; })]);
 
-				svg.selectAll(".layer")
+				self.svg.selectAll(".layer")
 					.data(layers)
 					.enter().append("path")
 					.attr("class", function(d){ /*console.log(d);*/ return 'layer-'+d.key;})
-					.attr("d", function(d) { return area(d.values); })
+					.attr("d", function(d) { return self.area(d.values); })
 					.attr("transform", "rotate(90), translate(0,-"+self.maxHeight+"), scale("+self.ratio+",1)");
 
 
