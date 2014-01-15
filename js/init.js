08e9e66d20bbe853ed56ae92a1a7be5fe0b13255
Justin Heideman <justin@fiddlyio.com>
2014-01-14 19:23:26 -0600

    update all js packages, d3 now has native requirejs support

--- js/init.js
+++ js/init.js
@@ -7,30 +7,30 @@ requirejs.config({
         "d3": "../vendor/d3/d3",
         "hammer": "../vendor/dist/hammer",
         "async": "../vendor/requirejs-plugins/src/async",
-        "goog":  "../vendor/requirejs-plugins/src/async" //,
+        "goog": "../vendor/requirejs-plugins/src/async" //,
         //"propertyParser":  "../vendor/requirejs-plugins/src/propertyParser"
-    },
-    "shim": {
-        "d3": {
-            exports: 'd3'
-        },
-    }
+    } //,
+    //"shim": {
+    //    "d3": {
+    //        exports: 'd3'
+    //   },
+    //}
 });
 
 /* --------------------------
  Load modules below
  ---------------------------- */
 //handles what happens when someone clicks on the share icons in a story
-requirejs(["app/fifty"], function (fifty) {
+requirejs(["app/fifty"], function(fifty) {
     fifty.init();
 });
 
-requirejs(["app/gpfv","app/travelStudy"], function(gpfv, travelStudy){
+requirejs(["app/gpfv", "app/travelStudy"], function(gpfv, travelStudy) {
     //generalProgram.init('section.s1');
     // gpfv.init('section.s2');  // overall
     // gpfv.init('section.s3');  // organizations
 
-    gpfv.init('section.s1','fv');
+    gpfv.init('section.s1', 'fv');
     //gpfv.init('section.s6');
     //travelStudy();
 });
