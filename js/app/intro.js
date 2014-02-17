12129e6375ed17b4cad501ea84395b9f12c2c7a9
Justin Heideman <justin@fiddlyio.com>
2014-02-17 14:58:08 -0600

    in progress work on intro

--- js/app/intro.js
+++ js/app/intro.js
@@ -12,6 +12,53 @@ define(["jquery", 'stellar'], function intro($, stellar) {
                 verticalOffset: 0
             });
 
+
+            $('#wrap').on('scroll', function() {
+                if (window.step === 0) {
+
+                    var toShow = $('.blurb[data-year-start="' + window.year + '"]');
+                    //var toHide = $('.blurb[data-year-end="' + window.year + '"]');
+
+                    $('.blurb').removeClass('shown');
+                    toShow.addClass('shown');
+                    //toHide.removeClass('shown');
+
+
+                    // $('#wrap').on('scroll', function() {
+                    //     if (window.step === 0) {
+
+                    //         // console.log('run scroll');
+                    //         //console.log(self.els);
+                    //         self.els.each(function() {
+                    //             var el = $(this);
+                    //             var startYear = +el.data('start');
+                    //             var endYear = +el.data('end');
+
+                    //             // only animate if we're in the right year range
+                    //             if (window.year >= startYear && window.year <= endYear) {
+                    //                 var yPos = -($('#wrap').scrollTop() / el.data('speed'));
+                    //                 var offset = $('section.s0').height() * ((100 - el.data('offset')) / 100);
+                    //                 if (!isNaN(offset)) {
+                    //                     yPos = yPos + offset;
+                    //                 }
+                    //                 var coords = '50% ' + yPos + 'px';
+                    //                 //console.log(coords);
+                    //                 el.css({
+                    //                     backgroundPosition: coords
+                    //                 });
+                    //             }
+
+                    //             //console.log(yPos);
+
+                    //         });
+
+                    //     }
+                    // });()
+
+
+                }
+            });
+
             //this.els = $('.spot').not('.no-animate');
 
             // $('#wrap').on('scroll', function() {
