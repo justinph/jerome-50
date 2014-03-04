be16904fd207c87365fac5e665cb04c0e883d97d
Justin Heideman <justin@fiddlyio.com>
2014-03-03 22:49:09 -0600

    update mockups

--- mockups/index.php
+++ mockups/index.php
@@ -8,7 +8,7 @@ if (!isset($_GET['num'])){
 }
 
 $next = $_GET['num'] + 1;
-if ($next == 10 ){
+if ($next == 7 ){
 	$next = 1;
 }
 
@@ -24,7 +24,7 @@ $nextUrl = $_SERVER['PHP_SELF'] ."?num=".$next;
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
 	<meta http-equiv="content-type" content="text/html; charset=macintosh" />
-	<title>Jerome Foundation 50th <?php echo $_GET['num'];?>/9</title>
+	<title>Jerome Foundation 50th <?php echo $_GET['num'];?>/6</title>
 	<style type="text/css">
 	body
 {
