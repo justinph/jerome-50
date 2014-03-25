e6ce7ef99ed315f87d6b1e0659e4096f254a5c0e
Justin Heideman <justin@fiddlyio.com>
2014-03-24 21:55:50 -0500

    fix for #2

--- js/app/travelStudy.js
+++ js/app/travelStudy.js
@@ -2,49 +2,13 @@ require(["jquery", "async!http://maps.google.com/maps/api/js?key=AIzaSyAHHC2slaW
     "use strict";
 
 
-    // function detectMobile() { 
-    //  if( navigator.userAgent.match(/Android/i)
-    //  || navigator.userAgent.match(/webOS/i)
-    //  || navigator.userAgent.match(/iPhone/i)
-    //  || navigator.userAgent.match(/iPad/i)
-    //  || navigator.userAgent.match(/iPod/i)
-    //  || navigator.userAgent.match(/BlackBerry/i)
-    //  || navigator.userAgent.match(/Windows Phone/i)
-    //  ){
-    //     return true;
-    //   }
-    //  else {
-    //     return false;
-    //   }
-    // }
-    
-    // $('#map-canvas').css({
-    //     height: $(window).height(),
-    //     top:  ($(window).height() )
-    // });
-
-    // var mapPosition = $(window).height(); //$("#map-canvas").offset().top;
-
-
-    // $('#wrap').scroll(function(){
-    //     var topOffset = $('.s3').offset().top * -1;
-        
-    //     //console.log(topOffset, mapPosition);
-
-    //     if (topOffset >= mapPosition ){
-    //         $("#map-canvas").addClass("fixed_map");
-    //     }
-
-    // }).scroll();
-
-
     var map, mapData;
     var mn = new google.maps.LatLng(46.4, -93.790039);
     var MY_MAPTYPE_ID = 'custom_style';
     var tableId = '1ZG7LLuMPO3NDMpwOZZ9WBV3613MpM2gDzvDeQog';
     var featureOpts = [{
             stylers: [{
-                hue: '#343434'
+                hue: '#646361'
             }, {
                 visibility: 'simplified'
             }, {
@@ -62,7 +26,7 @@ require(["jquery", "async!http://maps.google.com/maps/api/js?key=AIzaSyAHHC2slaW
         {
             featureType: 'water',
             stylers: [{
-                color: '#343434'
+                color: '#646361'
             }]
         }
     ];
@@ -113,15 +77,23 @@ require(["jquery", "async!http://maps.google.com/maps/api/js?key=AIzaSyAHHC2slaW
 
     var updateYear = function updateYear() {
 
-        if (window.year === 1963) {
+        if (window.year >= 1965) {
             $('#year').addClass('faded');
             $('#year-down').addClass('inactive');
-            $('.s3 .textDisplay').show();
+            $('.s3 .textDisplay').addClass('show');
         } else {
-            $('.s3 .textDisplay').hide();
+            $('.s3 .textDisplay').removeClass('show');
             $('#year-down, #year-up').removeClass();
             $('#year').removeClass().text(window.year);
         }
+
+        //hide show the element (not just transition) so we can click
+        if (window.year >= 1975){
+            $('.s3 .textDisplay').hide();
+        } else  {
+            $('.s3 .textDisplay').show();
+        }
+
 
         mapData.setOptions({
             query: {
@@ -146,19 +118,6 @@ require(["jquery", "async!http://maps.google.com/maps/api/js?key=AIzaSyAHHC2slaW
             if (window.year > 2013) {
                 window.year = 2013;
             }
-
-
-            // //when we hit 1970, scroll up
-            // if (window.year < 1968 && $("#map-canvas").hasClass('fixed_map') ){
-            //     $("#map-canvas").removeClass("fixed_map");
-            //     $('#wrap').scrollTop(100);
-            //     //console.log(parseInt((delta * 5), 10));
-                
-            //     $('#wrap').animate({
-            //         scrollTop: ($(window).height()-100)
-            //     }, 0);
-
-            // }
 
             updateYear();
         }
